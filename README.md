68201f17513748006108f6906503f8bfacf1f096
Emily Merwin <emilymerwinajc@gmail.com>
2013-09-23 20:09:49 -0400

    Readability and documentation
    Rename some vars to be more descriptive and add some comments for
    future reference

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Poll Display
 This ran 9/22/13 as an exclusive to myajc.com <a href="http://www.myajc.com/aca-georgia-poll/">here</a>. The stubbed version for ajc.com ran <a href="http://www.ajc.com/news/aca-georgia-poll-free/">here</a>.
-
+It is part of our series on the Affordable Care Act and Georiga, found <a href="http://www.myajc.com/s/news/healthcare-georgia/">here</a>.
 ###To do
 [ ] jQuery library is outdated but broke stuff when I tried to update it. jQuery UI library is current (as of 9/20/13)
 
@@ -10,13 +10,14 @@ This ran 9/22/13 as an exclusive to myajc.com <a href="http://www.myajc.com/aca-
 
 - note that the demographic group title cells often have a few leading spaces, which could break your python parser if you don't know they are there
 
-- numbers will come in as strings "100%" etc, need to convert before parsing (format --> normal), and python parser will convert to whole numbers
+- numbers will come in as strings "100%" etc, need to convert before parsing (format --> normal), and python parser will convert to whole numbers (assuming they are only 2 digits percent representations, haven't tested with numbers that need rounding)
+
 - ACA poll is using a column of edited "response" fields separate from the defaults, which are in the original .csv - parser checks for this and removes those that do not have text in the edited response field
 
 - make sure to remove "UNWEIGHTED BASE" and "TOTAL RESPONDENTS" from the file you give the parser, as these are not percentages but the actual number of people in each category
 
 - it comes over in all caps - next time try downloading, converting the data to downcase locally (find and replace regex probably easiest) then re-upload to Google and go through and make sure the appropriate things are uppercase (think proper nouns).
 
-- FYI the parser is set to replace "* " and "- " with in the data "0" because those characters were breaking stuff, and if the value is 0 that label won't show up anyway (because it won't have a pixel width, not because it's not added to the DOM, fix that maybe). I believe there are checks in the code to get rid of them but it was still breaking. The polling company uses those symbols to mean either 0 or not a large enough sample.
+- FYI the parser is set to replace "* ", "- " and " " with "0" because those characters were breaking stuff, and if the value is 0 that label won't show up anyway (because it won't have a pixel width, not because it's not added to the DOM, fix that maybe). I believe there are checks in the code to get rid of them but it was still breaking. The polling company uses those symbols to mean either 0 or not a large enough sample.
 
 	
--- js/poll.js
+++ js/poll.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
 });//document ready
 function parseQuestions(xml){
 	var labelArr = ["Statewide", "Atlanta metro", "Men", "Women", "White", "Non-white",  "Insured", "Uninsured"];
-	var buttonHTML = ""
+	var buttonHTML = "";
 	for(var i=0; i<labelArr.length; i++){
 		buttonHTML += '<input type="radio" id="radio'+i+'" name="radio" /><label for="radio'+i+'">'+labelArr[i]+'</label>';
 	}
 	document.getElementById('radio').innerHTML = buttonHTML;
 	$("#radio0").prop("checked", "checked");
-	$("#radio").buttonset();
+	$("#radio").buttonset();//initialize for jQueryUI
 	var bigArray = [];
 	var myQs = new Question();
 	function Question(){//creating an object to initiate so we can prototype it, even though we aren't doing that here
@@ -34,18 +34,18 @@ function parseQuestions(xml){
 				var option = new Object;
 				var temp = $(this).text();
 				tempArray = temp.split(',');
-				option.groupArr = [];
+				option.demogArr = [];
 				jQuery.each(tempArray, function(b, c){
-					option.groupArr[b] = +c;
+					option.demogArr[b] = +c;
 				});//convert to integer and add to demographic breakdown array
 				option.optlabel = optLabel;
 				bigArray[Qid][index] = option;
 				responseArray[index] = option;//maybe delete later
 			});//Response.each
-			//for (var u=0; u<responseArray.length -1; u++){//-1 only if you have totals as last row
-			for (var u=0; u<responseArray.length; u++){
-				for(var m=0;m<responseArray[u].groupArr.length;m++){
-					var temp = responseArray[u].groupArr[m];
+			for (var u=0; u<responseArray.length; u++){//-1 from .length if you have totals as your final row
+				//add up each demographic group, so we can use it later to force them to appear to be 100% to account for rounding
+				for(var m=0;m<responseArray[u].demogArr.length;m++){
+					var temp = responseArray[u].demogArr[m];
 					if(totalsArr[m]){
 						totalsArr[m] += temp;
 					} else{
@@ -57,23 +57,22 @@ function parseQuestions(xml){
 			if(Qid == 8){
 				$(myParent).append('<div class="question">Here are several elements of the Affordable Care Act.  For each one, please tell me whether your view of the provision is favorable or unfavorable.</div><div id="groupQ"></div>');
 			}
-			if(Qid >7){
+			if(Qid > 7){
 				myParent = '#groupQ';
 			}
 			$(myParent).append('<div id=q'+Qid+'></div>');
 			startup();
 
 			function loadResults (val){
-				//for(var g=0; g<responseArray.length -1; g++){//remove -1 if you don't have totals as your final row
-				for(var g=0; g<responseArray.length; g++){
-					opts[g] = responseArray[g].groupArr[val];
-					opts.totes = val;
+				for(var g=0; g<responseArray.length; g++){ //-1 from .length if you have totals as your final row
+					opts[g] = responseArray[g].demogArr[val]; //associate each demographic slice from each response with their corresponding filter button
+					opts.id = val;
 				}
 				drawBars();
 			}//loadResults*/
-			loadResults(0);
+			loadResults(0);//default to the first filter
 			//consolidate these
-			for(var w=0, myRadio; w < responseArray[0].groupArr.length; w++){
+			for(var w=0, myRadio; w < responseArray[0].demogArr.length; w++){
 				var myString = '#radio'+w;
 				var myRadio = $(myString);
 				myRadio.click(function(num){
@@ -83,22 +82,20 @@ function parseQuestions(xml){
 				}(w));
 			}
 			function startup(){
-				var otherHolder = '<div id="holderq'+Qid+'"><div id="questionq'+Qid+'" class="question"></div><div class="results">';
-				//for (var p=0; p<bigArray[Qid].responseArray.length -1; p++){//-1 only if you have last response row as total responses (so that you don't have to calculate total responses - useful for making sure all bars are the same length despite rounding)
-				for (var p=0; p<bigArray[Qid].responseArray.length; p++){
-					otherHolder += '<div class="opt'+p+'percent" id="opt'+p+'q'+Qid+'"><div class="innerlabel">'+bigArray[Qid][p].optlabel+'</div></div>'
+				var qText = '<div id="holderq'+Qid+'"><div id="questionq'+Qid+'" class="question"></div><div class="results">';
+				for (var p=0; p<bigArray[Qid].responseArray.length; p++){ //-1 from .length if you have totals as your final row (so that you don't have to calculate total responses - useful for making sure all bars are the same length despite rounding)
+					qText += '<div class="opt'+p+'percent" id="opt'+p+'q'+Qid+'"><div class="innerlabel">'+bigArray[Qid][p].optlabel+'</div></div>'
 				}
-				otherHolder += '</div></div>';
-				$('#q'+Qid).append(otherHolder);
-				$('#questionq'+Qid).append(q);
+				qText += '</div></div>';
+				$('#q'+Qid).append(qText);
 			}//startup
 			function drawBars(){
-				var multiplier = ($("#qs").width()-10)/100;
+				var multiplier = ($("#qs").width()-10)/100; //subtract 10 to give it enough buffer to prevent it overflowing its container when animating
 				for(var i=0; i<opts.length; i++){
 					if(Qid > 7){
 						multiplier = ($("#groupQ").width()-10)/100;
 					}
-					var t = multiplier*((100/bigArray[Qid].totalsArr[opts.totes]).toFixed(2));//scale it to account for rounding causing to be more or less than 100
+					var t = multiplier*((100/bigArray[Qid].totalsArr[opts.id]).toFixed(2)); //scale it to account for rounding causing to be more or less than 100//not sure we need the .toFixed(2) anymore now that our data uses whole numbers, this may have been to get around the .float rounding error
 					var barWidth = opts[i]*t;
 					$('#opt'+i+'q'+Qid).delay(100).animate({'width':barWidth+'px'},'slow').hover(function () {
 						$(this).css({'opacity':'0.7'});
