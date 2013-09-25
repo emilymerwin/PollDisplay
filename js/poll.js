132bc5d15a48af5274c5437c2bd6221f87717c36
Emily Merwin <emilymerwinajc@gmail.com>
2013-09-24 23:01:13 -0400

    Consolidate redundancies and reorganize var declarations
    Also changed all "*Array" to "*Arr" to have a consistent naming
    convention. Changed b's to strong's in the tooltip code.

--- js/poll.js
+++ js/poll.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
 		success: parseQuestions
 	});
 });//document ready
+
 function parseQuestions(xml){
+	var bigArr = [];
+	
+	//setup the radio button filters
 	var labelArr = ["Statewide", "Atlanta metro", "Men", "Women", "White", "Non-white",  "Insured", "Uninsured"];
-	var buttonHTML = "";
+	var buttonHTML = "";	
 	for(var i=0; i<labelArr.length; i++){
 		buttonHTML += '<input type="radio" id="radio'+i+'" name="radio" /><label for="radio'+i+'">'+labelArr[i]+'</label>';
 	}
-	
-	document.getElementById('filters').innerHTML = buttonHTML;
-	$("#filters").buttonset();//initialize for jQueryUI
-	var bigArray = [];
-
+	$(document.getElementById('filters')).html(buttonHTML).buttonset(); //.buttonset() initializes the radio buttons for jQueryUI
 	for(var w=0; w < labelArr.length; w++) {
 		$('#radio'+w).click(function(num) {
 			return function () {
@@ -25,36 +25,33 @@ function parseQuestions(xml){
 		}(w));
 	}
 
-	$(xml).find("myQuestion").each(function(index, element){
-		this.id = index;
-		bigArray[index] = this;
-		var Qid = $(this).attr("Qid");
-		qLabel = $(this).attr("label");
-		this.responseArray = [];
-		var totalsArr = [];
-		this.totalsArr = totalsArr;
+	$(xml).find("myQuestion").each(function(index){
+		this.responseArr = [];
+		this.totalsArr = [];
+		this.qLabel = $(this).attr("label");
+		var Qid = index;
 		
-		$(this).find("Response").each(function(index, element){
-			var optLabel = $(this).attr('Label');
+		bigArr[index] = this;
+
+		$(this).find("Response").each(function(index){
 			var option = new Object;
-			var temp = $(this).text();
-			tempArray = temp.split(',');
+			option.optlabel = $(this).attr('Label');
 			option.demogArr = [];
-			jQuery.each(tempArray, function(b, c){
+			tempArr = $(this).text().split(',');
+			jQuery.each(tempArr, function(b, c){
 				option.demogArr[b] = +c;
 			});//convert to integer and add to demographic breakdown array
-			option.optlabel = optLabel;
-			bigArray[Qid].responseArray[index] = option;
+			bigArr[Qid].responseArr[index] = option;
 		});//Response.each
 
-		for (var u=0; u<this.responseArray.length; u++){ //-1 from .length if you have totals as your final row
+		for (var u=0; u<this.responseArr.length; u++){ //-1 from .length if you have totals as your final row
 			//add up each demographic group, so we can use it later to force them to appear to be 100% to account for rounding
-			for(var m=0; m<this.responseArray[u].demogArr.length; m++){
-				var temp = this.responseArray[u].demogArr[m];
-				if(totalsArr[m]){
-					totalsArr[m] += temp;
+			for(var m=0; m<this.responseArr[u].demogArr.length; m++){
+				var temp = this.responseArr[u].demogArr[m];
+				if(this.totalsArr[m]){
+					this.totalsArr[m] += temp;
 				} else{
-					totalsArr[m] = temp;
+					this.totalsArr[m] = temp;
 				}
 			}
 		}//for
@@ -62,23 +59,13 @@ function parseQuestions(xml){
 		startup(Qid);
 		
 	});//xml.find(myQuestion)
+	
 	$("#radio0").click(); //default to the first filter
 	
-	function loadResults (val){
-		for(var a=0; a<bigArray.length; a++){
-			var opts = [];
-			for(var g=0; g<bigArray[a].responseArray.length; g++){ //-1 from .length if you have totals as your final row
-				opts[g] = bigArray[a].responseArray[g].demogArr[val]; //associate each demographic slice from each response with their corresponding filter button
-				opts.id = val;
-			}
-			drawBars(a, opts);
-		}
-	}//loadResults*/
-	
 	function startup(Qid){
-		var qText = '<div id=q'+Qid+'><div id="questionq'+Qid+'" class="question">'+qLabel+'</div><div class="results">';
-		for (var p=0; p<bigArray[Qid].responseArray.length; p++){ //-1 from .length if you have totals as your final row (so that you don't have to calculate total responses - useful for making sure all bars are the same length despite rounding)
-			qText += '<div class="opt'+p+'percent" id="opt'+p+'q'+Qid+'"><div class="innerlabel">'+bigArray[Qid].responseArray[p].optlabel+'</div></div>'
+		var qText = '<div id=q'+Qid+'><div id="questionq'+Qid+'" class="question">'+bigArr[Qid].qLabel+'</div><div class="results">';
+		for (var p=0; p<bigArr[Qid].responseArr.length; p++){ //-1 from .length if you have totals as your final row (so that you don't have to calculate total responses - useful for making sure all bars are the same length despite rounding)
+			qText += '<div class="opt'+p+'percent" id="opt'+p+'q'+Qid+'"><div class="innerlabel">'+bigArr[Qid].responseArr[p].optlabel+'</div></div>'
 		}
 		qText += '</div></div>';
 		var myParent = '#qs';
@@ -91,13 +78,24 @@ function parseQuestions(xml){
 		$(myParent).append(qText);
 	}//startup
 
+	function loadResults (val){
+		for(var a=0; a<bigArr.length; a++){
+			var opts = [];
+			for(var g=0; g<bigArr[a].responseArr.length; g++){ //-1 from .length if you have totals as your final row
+				opts[g] = bigArr[a].responseArr[g].demogArr[val]; //associate each demographic slice from each response with their corresponding filter button
+				opts.id = val;
+			}
+			drawBars(a, opts);
+		}
+	}//loadResults
+	
 	function drawBars(Qid, opts){
 		var multiplier = ($("#qs").width()-10)/100; //subtract 10 to give it enough buffer to prevent it overflowing its container when animating
 		for(var i=0; i<opts.length; i++){
 			if(Qid > 7){
 				multiplier = ($("#groupQ").width()-10)/100;
 			}
-			var t = multiplier*((100/bigArray[Qid].totalsArr[opts.id]).toFixed(2)); //scale it to account for rounding causing to be more or less than 100//not sure we need the .toFixed(2) anymore now that our data uses whole numbers, this may have been to get around the .float rounding error
+			var t = multiplier*((100/bigArr[Qid].totalsArr[opts.id]).toFixed(2)); //scale it to account for rounding causing to be more or less than 100//not sure we need the .toFixed(2) anymore now that our data uses whole numbers, this may have been to get around the .float rounding error
 			var barWidth = opts[i]*t;
 			$('#opt'+i+'q'+Qid).delay(100).animate({'width':barWidth+'px'},'slow').hover(function () {
 				$(this).css({'opacity':'0.7'});
@@ -105,7 +103,7 @@ function parseQuestions(xml){
 				$('#opt'+i+'q'+Qid).find("span:last").remove();
 				$(this).css({'opacity':'1.0'});
 			}).qtip({
-				content: bigArray[Qid].responseArray[i].optlabel+': <b>' + opts[i] +'%</b>',
+				content: bigArr[Qid].responseArr[i].optlabel+': <strong>' + opts[i] +'%</strong>',
 				position: {
 					my: 'top right',
 					target: 'mouse',
