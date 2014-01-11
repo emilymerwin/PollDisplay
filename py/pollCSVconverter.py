15a9cbf86837cf925d577d21b680ed7019007c7a
Emily Merwin <emilymerwinajc@gmail.com>
2014-01-10 23:58:41 -0500

    Add demographic cols by index instead of dict lookup
    Sometimes there are a lot of these. Save typing and reduce mistakes!

--- py/pollCSVconverter.py
+++ py/pollCSVconverter.py
@@ -3,40 +3,44 @@ import csv
 from xml.dom.minidom import Document
 import decimal
 
-data = csv.DictReader (open("pollLower.csv",'U'))
+data = csv.reader (open("../data/30019 GA Poll Banner Table - pollLower.csv",'U'))
 #Create the XML doc
 doc = Document()
 #create the base element
 docbase = doc.createElement("docbase")
 doc.appendChild(docbase)
 QuestionArray = []
+data.next()
 def checkstring(n):
-	stripped = n.strip()
+	stripped = n.strip() #sometimes the fields we're checking for below come through with whitespace attatched
 	if "-" == stripped or "*" == stripped or "" == stripped:
 		return "0"
 	else:
 		dec = int(decimal.Decimal(n)*100)
 		return str(dec)
 for row in data:
-	myID = int(row['qnum'])
-	Qid= row['qnum']
+	myAnswers = []
+	myID = int(row[0])
+	Qid= row[0]
 	if len(QuestionArray) <= myID:
 		myQuestion = doc.createElement('myQuestion')
 		myQuestion.setAttribute('Qid', Qid)
-		myQuestion.setAttribute('label', row['Question'])
+		myQuestion.setAttribute('label', row[1])
 		QuestionArray.append(myQuestion)
-	responses = row['Responses']
+	responses = row[2]
 	if responses != "": #data represented as pct in next row, skip over the actual number here (and add the answer label)
 		myResponse = doc.createElement('Response')
-		myResponse.setAttribute('Label', row['Responses'])
+		myResponse.setAttribute('Label', responses)
 		myQuestion.appendChild(myResponse)
 	else:
-		myAnswers = [checkstring(row['REGISTERED VOTERS']), checkstring(row['MALE']), checkstring(row['FEMALE']), checkstring(row['DEMOCRAT']), checkstring(row['REPUBLICAN']), checkstring(row['INDEPENDENT']), checkstring(row['WHITE NON-HISPANIC']), checkstring(row['NON-WHITE']), checkstring(row['18-39']), checkstring(row['40-64'])]
+		for i, v in enumerate(row):
+			if i > 2: #cols 0-2 are string fields
+				myAnswers.append(checkstring(v))
 		myString = "," #this will be the character that joins our list below
 		myResponse.appendChild(doc.createTextNode(myString.join(myAnswers)))
 
 	docbase.appendChild(myQuestion)
 
-f = open('poll.xml', 'w')
+f = open('../data/poll_leg2014.xml', 'w')
 doc.writexml(f, addindent=" ", newl="\n")
 f.close()
