1fde8c408073d08204fa62ed9181f36b642bde64
Emily Merwin <emilymerwinajc@gmail.com>
2014-05-09 19:22:26 -0400

    Remove unwanted filters

--- js/poll.js
+++ js/poll.js
@@ -11,7 +11,7 @@ function parseQuestions(xml){
 	var bigArr = [], buttonHTML = "", button2HTML = "",
 	//setup the radio button filters
 	// labelArr = ["Total", "Men", "Women", "Democrats", "Republicans", "Independents", "Support Deal", "Support Carter", "Whites", "Non-whites", "18-39", "40-64", "65+", "Under $50k", "$50k-$100k", "$100k or more", "Have children", "No children", "Atlanta metro", "Atlanta exurbs"];
-		labelArr = ["Total", "Very Likely Voters", "Male", "Female", "Democrat", "Republican", "Independent", "White", "Black", "18-39", "40-64", "65+", "White Protestant Evangelical", "North", "Atlanta Exurbs", "Southeast", "Southwest", "Under $50k", "$50k-$100k", "$100k or more", "Has gun", "No gun", "ACA Good", "ACA Bad"];
+		labelArr = ["Total", "Democrat", "Republican", "Independent", "White", "Black", "North Ga.", "Atlanta Exurbs", "Atlanta Metro", "Southeast Ga.", "Southwest Ga."];
 	for(var i=0; i<labelArr.length; i++){
 		buttonHTML += '<label class="btn btn-default"><input type="radio" name="radio" value='+i+' id="radio'+i+'"> '+labelArr[i]+'</label>';
 		button2HTML += '<label class="btn btn-default"><input type="radio" name="radio2" value='+i+' id="radioB'+i+'"> '+labelArr[i]+'</label>';
