d9e3d8dda115a93016adfcb57a8221c364e5e159
Emily Merwin <emilymerwinajc@gmail.com>
2019-12-05 17:21:46 -0500

    Update pollCSVconverter.py to python 3

--- py/pollCSVconverter.py
+++ py/pollCSVconverter.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# NOTE: This code is python 3 - change the path above if your path to python 3 is different
 import csv
 from xml.dom.minidom import Document
 import decimal
@@ -6,15 +7,13 @@ import decimal
 infile = "../data/april2019/poll-april-2019-good.csv" #path to the source CSV
 outfile = "april2019-poll.xml" #filename for the generated XML to be used 
 
-data = csv.reader (open(infile,'U'))
-
 #Create the XML doc
 doc = Document()
 #create the base element
 docbase = doc.createElement("docbase")
 doc.appendChild(docbase)
 QuestionArray = []
-data.next()
+
 def checkstring(n):
 	stripped = n.strip() #sometimes the fields we're checking for below come through with whitespace attatched
 	if "-" == stripped or "*" == stripped or "" == stripped:
@@ -22,28 +21,32 @@ def checkstring(n):
 	else:
 		dec = int(decimal.Decimal(n)*100)
 		return str(dec)
-for row in data:
-	myAnswers = []
-	myID = int(row[0])
-	Qid= row[0]
-	if len(QuestionArray) <= myID:
-		myQuestion = doc.createElement('myQuestion')
-		myQuestion.setAttribute('Qid', Qid)
-		myQuestion.setAttribute('label', row[1])
-		QuestionArray.append(myQuestion)
-	responses = row[2]
-	if responses != "": #data represented as pct in next row, skip over the actual number here (and add the answer label)
-		myResponse = doc.createElement('Response')
-		myResponse.setAttribute('Label', responses)
-		myQuestion.appendChild(myResponse)
-	else:
-		for i, v in enumerate(row):
-			if i > 2: #cols 0-2 are string fields
-				myAnswers.append(checkstring(v))
-		myString = "," #this will be the character that joins our list below
-		myResponse.appendChild(doc.createTextNode(myString.join(myAnswers)))
 
-	docbase.appendChild(myQuestion)
+with open(infile, 'rU', encoding='latin') as data:
+	reader = csv.reader(data, dialect='excel')
+	next(reader)
+	for row in reader:
+		myAnswers = []
+		myID = int(row[0])
+		Qid= row[0]
+		if len(QuestionArray) <= myID:
+			myQuestion = doc.createElement('myQuestion')
+			myQuestion.setAttribute('Qid', Qid)
+			myQuestion.setAttribute('label', row[1])
+			QuestionArray.append(myQuestion)
+		responses = row[2]
+		if responses != "": #data represented as pct in next row, skip over the actual number here (and add the answer label)
+			myResponse = doc.createElement('Response')
+			myResponse.setAttribute('Label', responses)
+			myQuestion.appendChild(myResponse)
+		else:
+			for i, v in enumerate(row):
+				if i > 2: #cols 0-2 are string fields
+					myAnswers.append(checkstring(v))
+			myString = "," #this will be the character that joins our list below
+			myResponse.appendChild(doc.createTextNode(myString.join(myAnswers)))
+
+		docbase.appendChild(myQuestion)
 
 f = open('../dist/' + outfile, 'w')
 doc.writexml(f, addindent=" ", newl="\n")
