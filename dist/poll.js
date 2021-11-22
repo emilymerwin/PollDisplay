e0c68ca545bce3b54369cf673c50f9c29fa3be52
Emily Merwin <emilymerwinajc@gmail.com>
2021-11-22 17:22:49 -0500

    Nov 2021 mayoral runoff poll

--- dist/poll.js
+++ dist/poll.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$.ajax({
 		type: "GET",
-		url: "oct2021-poll.xml", // TODO: Update with infile from settings
+		url: "nov2021-poll.xml", // TODO: Update with infile from settings
 		dataType: "xml",
 		success: parseQuestions
 	});
--- py/pollCSVconverter.py
+++ py/pollCSVconverter.py
@@ -4,8 +4,8 @@ import csv
 from xml.dom.minidom import Document
 import decimal
 
-infile = "../data/oct2021/poll-atl-mayor-oct-21.csv" #path to the source CSV
-outfile = "oct2021-poll.xml" #filename for the generated XML to be used 
+infile = "../data/nov2021/poll-atl-mayor-runoff-nov-2021.csv" #path to the source CSV
+outfile = "nov2021-poll.xml" #filename for the generated XML to be used 
 
 #Create the XML doc
 doc = Document()
